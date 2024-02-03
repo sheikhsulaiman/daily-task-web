7d4ebd5a23b23e610dc05796decc96e4086e55ff
sheikhsulaimansony <sheikhsulaimansony@outlook.com>
2024-02-03 16:05:46 +0600

    setup task ui [dummy data]

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,12 +1,12 @@
 import { Button } from "../ui/button";
 import { signOut } from "firebase/auth";
 import { auth } from "@/firebase/config";
+import TaskPage from "./TaskPage";
 
 const Home = () => {
-  const userName = auth.currentUser?.email;
   return (
     <>
-      <nav className="w-full flex items-center justify-between p-2 bg-yellow-200">
+      <nav className="w-full flex items-center justify-between p-2 bg-primary">
         <h1 className="font-bold text-2xl text-yellow-800">Daily Task</h1>
 
         <Button
@@ -19,9 +19,8 @@ const Home = () => {
           Sign Out
         </Button>
       </nav>
-      <main>
-        Home
-        {userName}
+      <main className="container mx-auto">
+        <TaskPage />
       </main>
     </>
   );
