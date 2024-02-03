cc3dfe6294db4f509bf324c12fa0ca6acab57d4e
sheikhsulaimansony <sheikhsulaimansony@outlook.com>
2024-02-03 16:10:30 +0600

    fix color

--- src/components/pages/Landing.tsx
+++ src/components/pages/Landing.tsx
@@ -5,8 +5,10 @@ import { ArrowUpRightFromSquareIcon } from "lucide-react";
 const Landing = () => {
   return (
     <>
-      <nav className="w-full flex items-center justify-between p-2 bg-yellow-200">
-        <h1 className="font-bold text-2xl text-yellow-800">Daily Task</h1>
+      <nav className="w-full flex items-center justify-between p-2 bg-primary">
+        <h1 className="font-bold text-2xl text-primary-foreground">
+          Daily Task
+        </h1>
         <Link to={"/app"}>
           <Button type="button">
             Get Started <ArrowUpRightFromSquareIcon className="h-4 w-4 ml-2" />
